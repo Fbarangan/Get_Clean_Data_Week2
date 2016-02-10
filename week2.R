e89a02c2251bbae496c24bad72af5bab75435310
Fbarangan <franb86.2@gmail.com>
2016-02-09 22:35:15 -0500

    commit 2

--- week2.R
+++ week2.R
@@ -29,3 +29,41 @@ library(RMySQL)
         affyMis <- fetch(query); quantile(affyMis$misMatches)
         affyMisSmall <- fetch(query, n=10) ; dbClearResult(query)
         dim(affyMisSmall)
+
+        dbDisconnect(hg19)
+
+#        http://cran.r-project.org/web/packages/RMySQL/RMySQL.pdf
+#
+#------------------ HDF5--------------------
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+library(rhdf5)
+created = h5createFile("example.h5")
+created
+# Create Group
+        created = h5createGroup("example.h5","foo")
+        created = h5createGroup("example.h5","baa")
+        created = h5createGroup("example.h5","foo/foobaa")
+        h5ls("example.h5")
+#Write to Group
+        A = matrix(1:10, nrow = 5, ncol = 2)
+  # write
+        h5write(A, "example.h5", "foo/A")
+        B = array(seq(0.1,2.0, by=0.1), dim = c(5,2,2))
+        attr(B,"scale") <- "liter"
+        h5write(B,"example.h5","foo/foobaa/B")
+        h5ls("example.h5")
+
+#Write a data set
+        df = data.frame(1L:5L, seq(0,1, length.out = 5),
+                c("ab","cde","fghi","a","s"), stringAsFactor = FALSE)
+        h5write(df, "example.h5", "df")
+        h5ls("example.h5")
+# Reading Data
+        readA = h5read("example.h5","foo/A")
+        readB = h5read("example.h5","foo/foobaa/B")
+        readdf = h5read("example.h5","df")
+
+# Writing and Reading chunks
+        h5write(c(12,13,14), "example.h5","foo/A", index= list(1:3,1))
+        h5read("example.h5","foo/A")
